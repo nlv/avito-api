e3bdff634def6bac7bb7cbb816a786cd1532ad6b
Leon Nikitin <nlv@lab321.ru>
2022-02-07 16:09:42 +0600

    Доступы в сторонние сервисы вынесены в параметры запуска приложения

--- api/exe/Server/Main.hs
+++ api/exe/Server/Main.hs
@@ -1,7 +1,25 @@
 
 module Main where
 
-import           App
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+import App
+
+optParser :: Parser Options
+optParser = Options 
+        <$> option auto ( long "port"       <> short 'p' <> help "Service port")
+        <*> strOption   ( long "dbname"     <> short 'b' <> help "Database name")
+        <*> strOption   ( long "dbuser"     <> short 'u' <> help "Database user")
+        <*> strOption   ( long "dbpassword" <> short 'w' <> help "Database password")
+        <*> strOption   ( long "s3url"      <> short 'l' <> help "S3 url")
+        <*> strOption   ( long "s3user"     <> short 'a' <> help "S3 user")
+        <*> strOption   ( long "s3skey"     <> short 'k' <> help "S3 secret key")
+        <*> strOption   ( long "s3region"   <> short 'r' <> help "S3 region")
 
 main :: IO ()
-main = run
+main = run =<< execParser opts
+    where opts = info (optParser <**> helper)
+                    ( fullDesc
+                    <> progDesc "Avitobot service"
+                    <> header "avitobot - avitobot service")
